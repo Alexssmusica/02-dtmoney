c013eb9f227eb1a71598a63905fc05514fca29db
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-11-07 17:37:02 -0300

    Criando estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
 	return (
-		<div className="App">
+		<>
+			<GlobalStyle/>
+
 			<h1>Hello World!!</h1>
-		</div>
+		</>
 	)
 }
 
